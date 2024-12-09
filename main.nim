fb94e8cd4d93d2cbce6b25e4e23059452af6c5dc
Maybe <mind2soumo@gmail.com>
2024-12-09 21:25:38 +0530

    good

--- main.nim
+++ main.nim
@@ -1,4 +1,4 @@
-import asynctools, telegrambot, modules/pin, modules/locks, modules/admin, db
+import asynctools, telegrambot, modules/pin, modules/locks, modules/admin, modules/afk, modules/filters, modules/greetings, modules/ban, modules/id, db
 import config
 
 let bot = newBot(config.BOT_TOKEN)
@@ -7,6 +7,11 @@ proc setupBot() =
   registerPinCommands(bot)
   registerLockCommands(bot)
   registerAdminCommands(bot)
+  registerFilterCommands(bot)
+  registerGreetingCommands(bot)
+  registerBanCommands(bot)
+  registerAFKCommands(bot)
+  registerIDCommand(bot)
 
 proc main() {.async.} =
   await bot.start()
